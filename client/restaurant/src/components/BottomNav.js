a4374bd470382113cf5d87e1fb7f60e6f717932f
Nam Doãn <dnam310199@gmail.com>
2020-03-30 11:46:30 -0700

    Bottom Nav Added

---
+++ client/restaurant/src/components/BottomNav.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { BottomNavigation, BottomNavigationAction } from "@material-ui/core"
+import HomeIcon from '@material-ui/icons/Home';
+import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    root: {
+        width: "100%",
+        position: "fixed",
+        bottom: 0,
+    },
+});
+
+export default function BottomNav() {
+    const classes = useStyles();
+    const [value, setValue] = React.useState('recents');
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+    return (
+        <BottomNavigation value={value} onChange={handleChange} showLabels className={classes.root}>
+            <BottomNavigationAction label="Home" icon={<HomeIcon />} />
+            <BottomNavigationAction label="Search by Price" icon={<AttachMoneyIcon />} />
+            <BottomNavigationAction label="Explore" icon={<LocationOnIcon />} />
+        </BottomNavigation>
+    )
+}
--- client/restaurant/src/components/Home.js
+++ client/restaurant/src/components/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import TopLogo from "./TopLogo"
 import SlideShow from "./SlideShow"
+import BottomNav from "./BottomNav"
 
 export default function Home() {
     return (
         <>
             <TopLogo />
             <SlideShow />
+            <BottomNav />
         </>
     );
 }
