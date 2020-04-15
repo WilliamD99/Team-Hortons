06a89205ea672e2a5b8025f39f63b750f4eb8f67
Nam Doãn <dnam310199@gmail.com>
2020-04-15 16:52:44 -0700

    WOP Marker

--- client/restaurant/src/components/Explore.js
+++ client/restaurant/src/components/Explore.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react'
 import { withGoogleMap, GoogleMap, Marker, withScriptjs } from "react-google-maps"
+import { InfoBox } from "react-google-maps/lib/components/addons/InfoBox"
+import { withProps, compose, withStateHandlers } from "recompose"
 
 export default class Explore extends Component {
     render() {
-        const GoogleMapExample = withScriptjs(withGoogleMap(props => (
+        const GoogleMapExample = compose(withProps({
+            googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyCZVJCN9TzYqSwQ_KBRX1d0C1h54abwg-0",
+            containerElement: <div style={{ height: `500px`, width: '500px' }} />,
+            mapElement: <div style={{ height: `100%` }} />,
+            loadingElement: <div style={{ height: `100%`, width: `100%` }} />,
+        })(withScriptjs(withGoogleMap(props => (
             <GoogleMap
                 defaultCenter={{ lat: 40.756795, lng: -73.954298 }}
                 defaultZoom={13}
             >
-                {props.isMarkerShown && <><Marker position={{ lat: -34.397, lng: 150.644 }} /><Marker position={{ lat: -50.397, lng: 150.644 }} /></>}
+
+                <>
+                    <Marker position={{ lat: -34.397, lng: 150.644 }} />
+                    <Marker position={{ lat: -50.397, lng: 150.644 }} />
+                </>
             </GoogleMap>
-        )));
+        )))));
         return (
             <div className="mt-3">
-                <GoogleMapExample
-                    containerElement={<div style={{ height: `500px`, width: '500px' }} />}
-                    mapElement={<div style={{ height: `100%` }} />}
-                    googleMapURL="https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyCZVJCN9TzYqSwQ_KBRX1d0C1h54abwg-0"
-                    loadingElement={<div style={{ height: `100%`, width: `100%` }} />}
-                    isMarkerShown
-                />
+                <GoogleMapExample>
+                </GoogleMapExample>
             </div>
         )
     }
